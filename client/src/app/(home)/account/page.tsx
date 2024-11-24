e9694ba7d5c832bf3999978d401dac8b83a8486e
Abel Misiocha <wells.mail.13@gmail.com>
2024-11-24 19:32:06 +0300

    feat(core): Add user wishlist

--- client/src/app/(home)/account/page.tsx
+++ client/src/app/(home)/account/page.tsx
@@ -1,12 +1,9 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Separator } from '@/components/ui/separator'
+import Chats from '@/components/dashboard/buyer/chats'
+import Wishlist from '@/components/dashboard/buyer/wishlist'
+import BuyerAccountSettings from '@/components/dashboard/buyer/account'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { Eye, EyeOff } from 'lucide-react'
-import { useState } from 'react'
 
 export default function Page() {
 	return (
@@ -32,7 +29,7 @@ export default function Page() {
 				</TabsList>
 
 				<TabsContent value='account'>
-					<Account />
+					<BuyerAccountSettings />
 				</TabsContent>
 				<TabsContent value='wishlist'>
 					<Wishlist />
@@ -43,83 +40,4 @@ export default function Page() {
 			</Tabs>
 		</section>
 	)
-}
-
-const Account = () => {
-	const [showPassword, setShowPassword] = useState(false)
-	return (
-		<div className='mt-4 overflow-hidden rounded-xl border-2 border-gray-200  bg-white sm:px-8'>
-			<div className='pt-4'>
-				<h1 className='py-2 font-manrope text-2xl font-semibold'>
-					Account settings
-				</h1>
-			</div>
-
-			<Separator className='my-4' />
-
-			<p className='py-2 font-manrope text-xl font-semibold'>
-				Email Address
-			</p>
-			<div className='flex flex-col sm:flex-row sm:items-center sm:justify-between'>
-				<p className='font-sans text-gray-600'>
-					Your email address is <strong>john.doe@company.com</strong>
-				</p>
-			</div>
-
-			<Separator className='my-4' />
-
-			<p className='font-manrope text-xl font-semibold'>Password</p>
-			<div className='mt-4 flex'>
-				<div className='flex flex-col space-y-2 sm:flex-row sm:space-x-3 sm:space-y-0'>
-					<Label htmlFor='login-password'>
-						<span className='text-sm text-gray-500'>
-							Current Password
-						</span>
-						<Input
-							placeholder='***********'
-							type={showPassword ? 'text' : 'password'}
-							className='mt-1 w-[300px]'
-						/>
-					</Label>
-					<Label htmlFor='login-password'>
-						<span className='text-sm text-gray-500'>
-							New Password
-						</span>
-						<Input
-							placeholder='***********'
-							type={showPassword ? 'text' : 'password'}
-							className='mt-1 w-[300px]'
-						/>
-					</Label>
-				</div>
-
-				<div
-					onClick={() => setShowPassword(!showPassword)}
-					className='mb-2 ml-2 mt-auto h-full cursor-pointer hover:opacity-80 active:opacity-100'
-				>
-					{showPassword ? <EyeOff /> : <Eye />}
-				</div>
-			</div>
-			<p className='mt-4'>
-				Can't remember your current password.&nbsp;
-				<Button
-					variant='link'
-					className='text-sm font-[500] text-blue-600 underline decoration-2'
-				>
-					Recover Account
-				</Button>
-			</p>
-			<Button variant='outline' className='mb-8 mt-4'>
-				Save Password
-			</Button>
-		</div>
-	)
-}
-
-const Wishlist = () => {
-	return <div className='mt-4'>Hello From Wishlist</div>
-}
-
-const Chats = () => {
-	return <div className='mt-4'>Hello From Chats</div>
 }
---
+++ client/src/components/dashboard/buyer/chats.tsx
@@ -0,0 +1,3 @@
+export default function Chats() {
+	return <div className='mt-4'>Hello From Chats</div>
+}
