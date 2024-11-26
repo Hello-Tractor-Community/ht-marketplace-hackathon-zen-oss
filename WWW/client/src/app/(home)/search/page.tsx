5132088aa4e5a3fc0aa98e3d28621327018298a7
Abel Misiocha <wells.mail.13@gmail.com>
2024-11-26 16:56:56 +0300

    feat(core): Nice update

--- WWW/client/src/app/(home)/search/page.tsx
+++ WWW/client/src/app/(home)/search/page.tsx
@@ -2,10 +2,21 @@
 
 import { DEFAULT_SLIDES } from '@/components/home/banner'
 import ProductSearchCard from '@/components/cards/search-card'
+import { useUserStore } from '@/store/user-store'
+import { useEffect } from 'react'
 
 export default function Search() {
+	const { queryParams } = useUserStore((state) => state)
+
+    async function handleSearch() {
+        // Make api call
+    }
+
+	useEffect(() => {
+        handleSearch()
+    }, [queryParams])
 	return (
-		<section className='px-4 md:container pb-32  pt-8 lg:px-32'>
+		<section className='px-4 pb-32 pt-8  md:container lg:px-32'>
 			<h1 className='text-2xl font-[500]'>Available tractors.</h1>
 			<div className='mt-6 grid grid-cols-1 gap-8 lg:grid-cols-2'>
 				{DEFAULT_SLIDES.map((slide, i) => {
